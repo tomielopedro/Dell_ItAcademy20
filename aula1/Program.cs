6f9b8a8e1aa2533783d81642a9c34816a2018e53
Tomielo <pedro.tomielo@dellteam.com>
2024-06-05 15:40:55 -0300

    aula 2

--- aula1/Program.cs
+++ aula1/Program.cs
@@ -1,4 +1,1 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-int? a = null;
-Console.WriteLine(a);
+﻿Console.WriteLine("Hello World");
--- aula2/Program.cs
+++ aula2/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace aula2;
+
+class Program
+{
+     static void Main(string[] args)
+     {
+        int n1 = default;
+        int? n1Null = default;
+
+        DateTime date = default;
+        DateTime? dateNull = default;
+
+        System.Console.WriteLine($"N1 sem ? {n1}");
+        System.Console.WriteLine($"N1 com ? {n1Null}");
+
+        System.Console.WriteLine($"Date sem ? {date}");
+        System.Console.WriteLine($"Date com ? {dateNull}");
+        
+
+     }
+
+}
